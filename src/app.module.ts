7b3872f5ef3b011fbad8c927f250561574ac36f9
David Foley <34948351+df2112@users.noreply.github.com>
2025-05-06 18:42:43 -0400

    feat: sync with official github sample

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { CatsModule } from './cats/cats.module';
 import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(CatsController);
+  }
+}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,18 @@
-
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { CreateCatDto } from './dto/create-cat.dto';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
 
+@UseGuards(RolesGuard)
 @Controller('cats')
 export class CatsController {
-  constructor(private catsService: CatsService) {}
+  constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  @Roles(['admin'])
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
@@ -16,5 +20,14 @@ export class CatsController {
   @Get()
   async findAll(): Promise<Cat[]> {
     return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id', new ParseIntPipe())
+    id: number,
+  ) {
+    // Retrieve a Cat instance by ID
+    console.log(id);
   }
 }
